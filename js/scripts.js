eb001d7e02d5d0c96503b093f4afb8fddaa71f9b
Francine Santos <francinesantoss85@gmail.com>
2023-08-30 10:55:45 -0300

    Create scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,107 @@
+
+// Objeto para pegar os preços e as fotos das camisetas
+
+var camisetas = {
+    'branca': {
+        
+        'gola_v': {
+            'sem_estampa': {
+                'preco_unit': 5.12,
+                'foto': 'v-white.jpg' 
+            },
+            'com_estampa': {
+                'preco_unit': 8.95,
+                'foto': 'v-white-personalized.jpg' 
+            }
+        },
+        
+        'gola_normal': {
+            'sem_estampa': {
+                'preco_unit': 4.99,
+                'foto': 'normal-white.jpg' 
+            },
+            'com_estampa': {
+                'preco_unit': 8.77,
+                'foto': 'normal-white-personalized.jpg' 
+            }
+        }
+    },
+    
+    'colorida': {
+        'gola_v': {
+            'sem_estampa': {
+                'preco_unit': 6.04,
+                'foto': 'v-color.jpg' 
+            },
+            'com_estampa': {
+                'preco_unit': 9.47,
+                'foto': 'v-color-personalized.png' 
+            }
+        },
+        
+        'gola_normal': {
+            'sem_estampa': {
+                'preco_unit': 5.35,
+                'foto': 'normal-color.jpg' 
+            },
+            'com_estampa': {
+                'preco_unit': 9.28,
+                'foto': 'normal-color-personalized.jpg' 
+            }
+        }
+    }
+}
+
+
+// parâmetros da pesquisa
+
+var parametros_pesquisa = {
+    "quantidade": 10,
+    "cor": "colorida",
+    "gola": "gola_v",
+    "qualidade": "q150",
+    "estampa": "com_estampa",
+    "embalagem": "bulk"
+}
+
+
+
+
+$(function(){
+
+    function atualizarOrcamento(parametros){
+        $('.refreshLoader').show();
+
+        var quantidade = parametros.quantidade;
+        var precoUnitario = camisetas[parametros.cor][parametros.gola][parametros.estampa].preco_unit;
+        var foto = "img/"+camisetas[parametros.cor][parametros.gola][parametros.estampa].foto;
+
+        var valorTotal = quantodade * preco_unit;
+        console.log(valorTotal);
+    }
+
+
+
+
+
+    // ao carregar página
+
+
+    // verificar local storeage e atualizar a variável parametrosPesquisa
+    atualizarOrcamento(parametros_pesquisa)
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
